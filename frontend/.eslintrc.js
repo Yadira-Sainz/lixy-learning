6b5b9ba2bd3710ddc163e1f09dffe57f09adf328
Yadira-Sainz <sain_06@outlook.com>
2024-09-26 22:55:45 -0600

    fix eslint version

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,10 +5,10 @@ module.exports = {
       'plugin:@typescript-eslint/recommended',
     ],
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off', // Disable no-unused-vars rule
-      '@typescript-eslint/no-empty-interface': 'off', // Disable no-empty-interface rule
-      'react-hooks/exhaustive-deps': 'warn', // Change to warning instead of error
-      'next/no-img-element': 'warn', // Change to warning instead of error
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'next/no-img-element': 'warn',
     },
   };
   
