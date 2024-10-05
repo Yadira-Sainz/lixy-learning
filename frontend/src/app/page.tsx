7a8823ca169d04a3093d42cf73993b257d9f2197
jenarv <jenniferarellanovi@gmail.com>
2024-10-05 08:55:43 -0600

    Save work in progress

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
 import LandingPage from '@/components/landing-page'
-import Navbar from '@/components/navbar'
 import FeaturesPage from '@/components/features-page'
 import ContactPage from '@/components/contact-page'
-import Footer from '@/components/footer'
 
 export default function Home() {
   return(
