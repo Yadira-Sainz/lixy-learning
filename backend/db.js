744986000b41e936bfecd0797d1aaa8508a1b526
Alma <127154272+AlmaRodri@users.noreply.github.com>
2024-10-09 19:21:27 -0600

    api imagen y actualizacion a vocabulary

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,11 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  user: process.env.POSTGRES_USER,
+  url: process.env.DATABASE_URL,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
 });
 
 module.exports = pool;
